eb33a81788c7b178c4b772b35f77d6de3287850a
navfal-dev <navfal.dev@gmail.com>
2021-08-13 23:55:43 +0530

    map iterate example

--- mergeSortedLists.js
+++ mergeSortedLists.js
@@ -1,3 +1,25 @@
+let recipeMap = new Map([
+    ['cucumber', 500],
+    ['tomatoes', 350],
+    ['onion', 50]
+]);
+
+/*
+// iterate over keys (vegetables)
+for (let vegetable of recipeMap.keys()) {
+    alert(vegetable); // cucumber, tomatoes, onion
+}
+
+// iterate over values (amounts)
+for (let amount of recipeMap.values()) {
+    alert(amount); // 500, 350, 50
+}
+
+// iterate over [key, value] entries
+for (let entry of recipeMap) { // the same as of recipeMap.entries()
+    alert(entry); // cucumber,500 (and so on)
+}
+*/
 
 const mergeTwoLists = (m, n) => {
     let p1 = 0, p2 = 0,
